378a0ad3d3f50f8edca55bc8c3b28e99b928a38a
Jonathan Ballard <asgardschosen@gmail.com>
2020-04-13 13:26:47 -0700

    done

---
+++ apps/survey/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+def result(request):
+    if request.method == 'POST':
+        print(request.POST)
+        context = {
+            "name":request.POST["name"],
+            "location":request.POST["location"],
+            "language":request.POST["language"],
+            "comment":request.POST["comment"],
+
+        }
+        return render(request, 'result.html', context)
+
